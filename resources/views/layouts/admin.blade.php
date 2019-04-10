13822e5d47d5883504deb64d9e8c4c3aaf059bb9
vagrant <vagrant@homestead>
2019-04-10 20:45:14 +0000

    integrando layout de admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -16,20 +16,27 @@
   <link href="{{ asset('admin/css/styles.css') }}" rel="stylesheet">
   <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}" type="image/x-icon">
 
-  <title>SB Admin - Blank Page</title>
+  <title>Lean Associates</title>
 
+  <!-- Scripts -->
+  <script>
+        window.Laravel = <?php echo json_encode([
+            'csrfToken' => csrf_token(),
+        ]); ?>
+  </script>
 </head>
 
 <body id="page-top">
 
   <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
 
-    <a class="navbar-brand mr-1" href="index.html">Start Bootstrap</a>
+    <a class="navbar-brand mr-1" href="/home"><img src="{{ asset('img/logo-lean2.png') }}" width="40"/><span class="ml-1 text-default">Lean Associates</span></a>
 
     <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
       <i class="fas fa-bars"></i>
     </button>
-
+    
+    @if (Auth::check()) 
     <!-- Navbar Search -->
     <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
       <div class="input-group">
@@ -41,7 +48,8 @@
         </div>
       </div>
     </form>
-
+    
+     
     <!-- Navbar -->
     <ul class="navbar-nav ml-auto ml-md-0">
       <li class="nav-item dropdown no-arrow mx-1">
@@ -80,11 +88,30 @@
         </div>
       </li>
     </ul>
+    
+    @endif
+    
+    @if (Auth::guest())  
+
+      <div class="collapse navbar-collapse justify-content-end">
+        <ul class="navbar-nav ml-auto ml-md-0">
+          <li class="nav-item">
+            <a class="nav-link" href="{{ url('/login') }}">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ url('/register') }}">Cadastro</a>
+          </li>
+        </ul>
+      </div>
+    
+    @endif
+
 
   </nav>
 
   <div id="wrapper">
-
+    
+    @if (Auth::check())  
     <!-- Sidebar -->
     <ul class="sidebar navbar-nav">
       <li class="nav-item">
@@ -121,10 +148,13 @@
       </li>
     </ul>
 
+    @endif
+
     <div id="content-wrapper">
-
+    
       <div class="container-fluid">
-
+        
+        @if (Auth::check())  
         <!-- Breadcrumbs-->
         <ol class="breadcrumb">
           <li class="breadcrumb-item">
@@ -133,22 +163,25 @@
           <li class="breadcrumb-item active">Blank Page</li>
         </ol>
 
+        @endif
+
         <!-- Page Content -->
 
          @yield('content')
 
       </div>
       <!-- /.container-fluid -->
-
+      
+       @if (Auth::check())  
       <!-- Sticky Footer -->
       <footer class="sticky-footer">
-        <div class="container my-auto">
+        <div class="container-fluid my-auto">
           <div class="copyright text-center my-auto">
             <span>Copyright © Your Website 2019</span>
           </div>
         </div>
       </footer>
-
+      @endif 
     </div>
     <!-- /.content-wrapper -->
 
@@ -165,20 +198,27 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+          <h5 class="modal-title" id="exampleModalLabel">Realmente deseja sair?</h5>
           <button class="close" type="button" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span>
           </button>
         </div>
-        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+        <div class="modal-body">Selecione "Logout" para terminar com sua sessão atual</div>
         <div class="modal-footer">
           <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="login.html">Logout</a>
+          <a class="btn btn-primary" href="{{ url('/logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;"> 
+                {{ csrf_field() }} 
+            </form> 
         </div>
       </div>
     </div>
   </div>
-
+  
+  @stack('applogin')
+  @stack('appregister')
+  @stack('appemail')
+  @stack('appresetpassword')
   <script src="{{ asset('admin/js/scripts.js') }}"></script>
 </body>
 
