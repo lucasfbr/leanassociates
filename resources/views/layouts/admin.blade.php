92971046fb597ea51b86f4245079878aa8aaa1eb
vagrant <vagrant@homestead>
2019-04-11 20:32:12 +0000

    inicio dashboard

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -135,17 +135,6 @@
     
       <div class="container-fluid">
         
-        @if (Auth::check())  
-        <!-- Breadcrumbs-->
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item">
-            <a href="index.html">Dashboard</a>
-          </li>
-          <li class="breadcrumb-item active">Blank Page</li>
-        </ol>
-
-        @endif
-
         <!-- Page Content -->
 
          @yield('content')
