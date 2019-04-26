f45dc0ef2d1dc4f4c7888fee5f525f9867d05149
vagrant <vagrant@homestead>
2019-04-26 14:08:51 +0000

    route profile

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,8 @@ Route::group(['middleware' => 'auth', 'prefix' => 'admin', 'as' => 'admin.'], fu
     Route::get('/usuarios/detail/{id}', 'admin\UsuarioController@detail')->name('usuarios.detail');
     Route::get('/usuarios/delete/{id}', 'admin\UsuarioController@delete')->name('usuarios.delete');
 
+    Route::get('/profile', 'admin\ProfileController@index')->name('profile');
+
     Route::get('/servicos', 'admin\ServicoController@index')->name('servicos');
     Route::get('/servicos/create', 'admin\ServicoController@create')->name('servicos.create');
     Route::post('/servicos/store', 'admin\ServicoController@store')->name('servicos.store');
